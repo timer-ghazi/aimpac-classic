d5617d478c21a5a9e7337b9bcaa36301a2778dc5
Qadir Timerghazin <qadir.timerghazin@gmail.com>
2025-07-02 17:29:22 -0500

    Added missing file

---
+++ gridv_memory.f90
@@ -0,0 +1,27 @@
+! Memory management routines for GRIDV
+
+SUBROUTINE allocate_grid(grid_size)
+   USE grid_data
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: grid_size
+   INTEGER :: ierr
+   
+   ! Deallocate if already allocated
+   IF (ALLOCATED(GRD)) DEALLOCATE(GRD)
+   
+   ! Allocate grid array
+   ALLOCATE(GRD(grid_size * grid_size), STAT=ierr)
+   IF (ierr /= 0) STOP 'Memory allocation failed for GRD'
+   
+   ! Update size variable
+   MPTS = grid_size
+   
+END SUBROUTINE allocate_grid
+
+SUBROUTINE cleanup_grid()
+   USE grid_data
+   IMPLICIT NONE
+   
+   IF (ALLOCATED(GRD)) DEALLOCATE(GRD)
+   
+END SUBROUTINE cleanup_grid
